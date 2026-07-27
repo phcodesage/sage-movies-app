20e5d0f9c08efbf4395b8bd7d04df1c651a488c5
Rechcel Toledo <rechceltoledo@gmail.com>
2026-07-27 21:40:31 +0800

    fix: update local studio assets with verified TMDB CDN images

--- lib/widgets/studio_bottom_bar.dart
+++ lib/widgets/studio_bottom_bar.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sagemovies/models/movie.dart';
-import 'package:sagemovies/screens/details_screen.dart';
-import 'package:sagemovies/services/api_service.dart';
 
 class StudioBottomBar extends StatelessWidget {
   final Function(String query)? onStudioTap;
@@ -9,25 +6,25 @@ class StudioBottomBar extends StatelessWidget {
   const StudioBottomBar({super.key, this.onStudioTap});
 
   static const List<Map<String, String>> studios = [
-    {'name': 'Vivamax', 'url': 'https://image.tmdb.org/t/p/w92/149142.png', 'query': 'Vivamax'},
-    {'name': 'Netflix', 'url': 'https://image.tmdb.org/t/p/w92/wwemzKWXHqRERyuR2VvM1yA4225.png', 'query': 'Netflix'},
-    {'name': 'Disney+', 'url': 'https://image.tmdb.org/t/p/w92/7xOCo2g1u4n6p425d0xQ.png', 'query': 'Disney+'},
-    {'name': 'Prime Video', 'url': 'https://image.tmdb.org/t/p/w92/if1Q8Tewh688f28j5t66u2j3441.png', 'query': 'Prime Video'},
-    {'name': 'Apple TV+', 'url': 'https://image.tmdb.org/t/p/w92/6vB2F9c4e0p9G6j96j40j4.png', 'query': 'Apple TV+'},
-    {'name': 'HBO Max', 'url': 'https://image.tmdb.org/t/p/w92/1DSpQ9G6537756f4d1e2.png', 'query': 'HBO Max'},
-    {'name': 'Paramount+', 'url': 'https://image.tmdb.org/t/p/w92/8342478f2441.png', 'query': 'Paramount+'},
-    {'name': 'Hulu', 'url': 'https://image.tmdb.org/t/p/w92/pqUtCleNUiTLLGsWyR62C92W67j.png', 'query': 'Hulu'},
-    {'name': 'Warner Bros', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Warner_Bros_logo.svg/512px-Warner_Bros_logo.svg.png', 'query': 'Warner Bros'},
-    {'name': 'Marvel', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Marvel-Studios-1-1.svg/512px-Marvel-Studios-1-1.svg.png', 'query': 'Marvel'},
-    {'name': 'Universal', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Universal_Pictures_logo.svg/512px-Universal_Pictures_logo.svg.png', 'query': 'Universal'},
-    {'name': 'Sony', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Columbia_Pictures_logo.svg/512px-Columbia_Pictures_logo.svg.png', 'query': 'Sony'},
-    {'name': 'A24', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/A24_logo.svg/512px-A24_logo.svg.png', 'query': 'A24'},
+    {'name': 'Vivamax', 'url': 'https://image.tmdb.org/t/p/w92/25oYoXHsfWYlddAzJSBReajN3BM.png', 'query': 'Vivamax'},
+    {'name': 'Netflix', 'url': 'https://image.tmdb.org/t/p/w92/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg', 'query': 'Netflix'},
+    {'name': 'Disney+', 'url': 'https://image.tmdb.org/t/p/w92/97yvRBw1GzX7fXprcF80er19ot.jpg', 'query': 'Disney+'},
+    {'name': 'Prime Video', 'url': 'https://image.tmdb.org/t/p/w92/pvske1MyAoymrs5bguRfVqYiM9a.jpg', 'query': 'Prime Video'},
+    {'name': 'Apple TV+', 'url': 'https://image.tmdb.org/t/p/w92/mcbz1LgtErU9p4UdbZ0rG6RTWHX.jpg', 'query': 'Apple TV+'},
+    {'name': 'HBO Max', 'url': 'https://image.tmdb.org/t/p/w92/jbe4gVSfRlbPTdESXhEKpornsfu.jpg', 'query': 'HBO Max'},
+    {'name': 'Paramount+', 'url': 'https://image.tmdb.org/t/p/w92/fts6X10Jn4QT0X6ac3udKEn2tJA.jpg', 'query': 'Paramount+'},
+    {'name': 'Hulu', 'url': 'https://image.tmdb.org/t/p/w92/bxBlRPEPpMVDc4jMhSrTf2339DW.jpg', 'query': 'Hulu'},
+    {'name': 'Warner Bros', 'url': 'https://image.tmdb.org/t/p/w92/zhD3hhtKB5qyv7ZeL4uLpNxgMVU.png', 'query': 'Warner Bros'},
+    {'name': 'Marvel', 'url': 'https://image.tmdb.org/t/p/w92/hUzeosd33nzE5MStB42PioTJw15.png', 'query': 'Marvel'},
+    {'name': 'Universal', 'url': 'https://image.tmdb.org/t/p/w92/8lvHyhjr8oUKOOy2dKXoALWKdp0.png', 'query': 'Universal'},
+    {'name': 'Sony', 'url': 'https://image.tmdb.org/t/p/w92/b9HLm1GDP4j3PF9TZQercD0r8kg.jpg', 'query': 'Sony'},
+    {'name': 'A24', 'url': 'https://image.tmdb.org/t/p/w92/u7nsZM0vmBMDpzIy27164AwMjXV.png', 'query': 'A24'},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 54,
+      height: 52,
       decoration: BoxDecoration(
         color: const Color(0xFF141419).withOpacity(0.95),
         border: const Border(
